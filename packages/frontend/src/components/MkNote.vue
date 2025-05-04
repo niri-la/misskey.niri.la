b6243bd8bed128855651d8033193e952d08c0891
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-05-04 19:41:48 +0900

    revert unrelated changes

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -769,6 +769,10 @@ function emitUpdReaction(emoji: string, delta: number) {
 	& + .article {
 		padding-top: 8px;
 	}
+
+	> .colorBar {
+		height: calc(100% - 6px);
+	}
 }
 
 .renoteAvatar {
@@ -838,6 +842,16 @@ function emitUpdReaction(emoji: string, delta: number) {
 	position: relative;
 	display: flex;
 	padding: 28px 32px;
+}
+
+.colorBar {
+	position: absolute;
+	top: 8px;
+	left: 8px;
+	width: 5px;
+	height: calc(100% - 16px);
+	border-radius: 999px;
+	pointer-events: none;
 }
 
 .avatar {
@@ -1062,6 +1076,13 @@ function emitUpdReaction(emoji: string, delta: number) {
 				margin-right: 12px;
 			}
 		}
+	}
+
+	.colorBar {
+		top: 6px;
+		left: 6px;
+		width: 4px;
+		height: calc(100% - 12px);
 	}
 }
 
