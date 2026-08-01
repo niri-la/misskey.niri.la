da72edea4341f5c357d30c1aacdeb912792105d3
anatawa12 <anatawa12@icloud.com>
2026-08-02 02:46:31 +0900

    chore: return reactionsCount with dummy value

--- packages/backend/src/server/api/endpoints/stats.ts
+++ packages/backend/src/server/api/endpoints/stats.ts
@@ -101,9 +101,9 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 				originalNotesCount,
 				usersCount,
 				originalUsersCount,
-				//reactionsCount,
 				//originalReactionsCount,
 				instances,
+				reactionsCount: 0,
 				driveUsageLocal: 0,
 				driveUsageRemote: 0,
 			};
